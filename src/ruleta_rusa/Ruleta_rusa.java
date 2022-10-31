9092bc4828350df27d27c8703bb1cda820134991
astridm18 <astridmarquina188@gmail.com>
2022-10-31 15:09:39 -0400

    cambios en clase principal

--- src/ruleta_rusa/Ruleta_rusa.java
+++ src/ruleta_rusa/Ruleta_rusa.java
@@ -3,12 +3,19 @@ package ruleta_rusa;
 
 
 public class Ruleta_rusa {
-    public static void main(String[] args) {
-        Jugadores Nombre = new Jugadores (1);
-        System.out.println(Nombre.isVivo());
-          Nombre.creararchivoregist("ganadores.out");
-          Nombre.creararchivoregist("perdedores.out");
-    }  
-    
-    
+    public static void main(String[] args) {      
+           //lista circular jugadores
+    Listajugadores_ listaJugadores = new Listajugadores_();
+    //crear jugador
+    Jugadores Nombre = new Jugadores ();
+    //jugador en lista circular
+    listaJugadores.Ingresenodo(Nombre);
+    //mostrar lista
+    listaJugadores.Desplegarnodo();
+         System.out.println(Nombre.isVivo());
+         Nombre.creararchivoregist( "ganadores.out");
+         Nombre.creararchivoregist( "perdedores.out");
+             
+    } 
 }
+
